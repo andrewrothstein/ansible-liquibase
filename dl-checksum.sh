28ba14f04dcc6e07402443588f71ca5d1ef0aca9
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-04 18:41:33 -0500

    updated for new packaging in 3.8+

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,13 +1,20 @@
 #!/bin/env sh
-VER=3.8.0
-MIRROR=https://github.com/liquibase/liquibase/releases/download
-TGZ=liquibase-${VER}-bin.tar.gz
-URL=$MIRROR/liquibase-parent-${VER}/${TGZ}
 DIR=~/Downloads
-LFILE=$DIR/$TGZ
-if [ ! -e $LFILE ]; then
-    wget -q -O $LFILE $URL
-fi
 
-printf "  # %s\n" $URL
-printf "  '%s': sha256:%s\n" $VER `sha256sum $LFILE | awk '{print $1}'`
+# https://github.com/liquibase/liquibase/releases/download/v3.8.4/liquibase-3.8.4.tar.gz
+MIRROR=https://github.com/liquibase/liquibase/releases/download
+
+dl_ver() {
+    local ver=$1
+    local file=liquibase-${ver}.tar.gz
+    local url=$MIRROR/v${ver}/${file}
+    local lfile=$DIR/$file
+    if [ ! -e $lfile ]; then
+        wget -q -O $lfile $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver ${1:-3.8.4}
