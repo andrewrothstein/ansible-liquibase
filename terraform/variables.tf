a7d6bbd27d4bdd6e2c8103d670f6cb71bc287c8e
Alejandro Alvarez <avazquez@liquibase.com>
2024-03-19 10:03:49 +0100

    feat(test.yml): add GitHub Actions workflow for deploying AWS infra a… (#1)
    * feat(test.yml): add GitHub Actions workflow for deploying AWS infra and testing role
    feat(main.tf): add Terraform configuration for provisioning AWS instances and key pair
    feat(outputs.tf): add output for public IP addresses of AWS instances
    feat(variables.tf): add variable for public key used for SSH access
    feat(versions.tf): add Terraform version and required providers configuration
    
    * chore(build.yml): comment out the entire 'jobs' section in the build workflow file to disable the bake-ansible-images-v1 job for now

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "public_key" {
+  description = "The public key to use for SSH access"
+  type        = string
+  // From datical-aws.pem
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHu7/QK1alb4qIWV2NAxgnVo1dE81n4wpy0yWcmqoOM4Mtf0Y1g9QOqyOK36mKeEc5y70cZvcWsHpgT+l31o9Roi11EdTWWHlkHt3wDYRwN0OzUHuLb3sUG1q+sfz+/AKgLe96XTDqEHWJzYL/MWb1oC9UKegk0svPnorl/yYNuFLjQuvUXP0WmQzDMAH6plaRGN1tIBVnqzPywio7W2akimkXTA8fb3qjH//YAYLuOitH9B8M6xrFmdDkRHUHiAIwkx6lFALZi++OXh/DjAtfKvBVmDyZOxE8qOQ5PjKEOX/Pj/KAFGX2NfLIQnZB8OphHpZPTckzZs9RkGA6XG6r"
+}
